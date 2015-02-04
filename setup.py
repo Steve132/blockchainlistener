9934bb4078fb54efb7127eaecf07a25dbcddd7f9
Steven Braeger <steve@soapforge.com>
2015-02-04 18:01:01 -0500

    Added setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+P=['blockchainlistener','blockchainlistener.backends','blockchainlistener.backends.blockchain_info']
+
+setup(name='blockchainlistener',
+      version='0.8',
+      description='Simple Blockchain Event Listener',
+      author='Steven Braeger',
+      author_email='Steve132@github.com',
+      url='https://github.com/Steve132/blockchainlistener',
+      packages=P,
+      requires=['pybitcointools']
+     )
